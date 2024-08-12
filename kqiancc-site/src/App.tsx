650702d255d04ddef016751ce9730d8cef8f1723
kqiancc <kqiancc@gmail.com>
2024-08-12 19:22:02 -0400

    removed unused import

--- kqiancc-site/src/App.tsx
+++ kqiancc-site/src/App.tsx
@@ -1,23 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home.tsx';
-import Parking from './pages/ParkingBuster.tsx';
+
 
 function App() {
   return (
     <Routes>
-      {/* <Timer /> */}
-       
-
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/second">Second Page</Link>
-            </li>
-          </ul>
-        </nav> */}
+      
       <Route index element={<Home/>}/>
       <Route path='home' element={<Home/>} />
     </Routes>
