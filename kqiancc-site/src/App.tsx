aef9d2b85cb5c96feeb8272cd19418cc63770213
kqiancc <kqiancc@gmail.com>
2024-08-11 21:26:21 -0400

    background color change

--- kqiancc-site/src/App.tsx
+++ kqiancc-site/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import tigerLogo from './assets/tiger.jpeg'
+
 import './App.css'
 
 function App() {
@@ -8,16 +8,11 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <div> 
+     
+      <h1>Hi! I'm Katherine Chacon Cai, </h1>
+      <h2>a UVA Student majoring in CS and minoring in English.</h2>
+      
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,9 +20,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
