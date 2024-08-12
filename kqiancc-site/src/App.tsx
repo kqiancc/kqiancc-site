9fdc324cc8af83d70cd4da7383d6994a99e71265
kqiancc <kqiancc@gmail.com>
2024-08-11 21:30:22 -0400

    the roughest of drafts

--- kqiancc-site/src/App.tsx
+++ kqiancc-site/src/App.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import tigerLogo from './assets/tiger.jpeg'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div> 
+      <div > 
      
       <h1>Hi! I'm Katherine Chacon Cai, </h1>
       <h2>a UVA Student majoring in CS and minoring in English.</h2>
+      <h3>WRITE A PARAGRAPH ABOUT MEEEEEE</h3>
       
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          resume
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        
+        
       </div>
       
     </>
