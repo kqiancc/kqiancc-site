a28d360e71b5dbfda53f37cf544a6e02d160f1d4
kqiancc <kqiancc@gmail.com>
2024-08-13 16:20:50 -0400

    hover font bold

--- kqiancc-site/src/pages/Home.tsx
+++ kqiancc-site/src/pages/Home.tsx
@@ -4,8 +4,9 @@ import Resume from '../components/resume.pdf';
 const HomePage: React.FC = () => {
   return (
     <div className='flex items-center justify-center h-screen font-mono bg-blue-200'>
-          <div className="w-full max-w-screen-lg px-4 mx-auto">
-            <h1 className="py-6 text-4xl text-purple-500 sm:text-5xl md:text-6xl">Hi, I'm Katherine Chacon Cai!</h1>
+          <div className="w-full max-w-screen-lg px-4 mx-auto ">
+            <p className="py-6 text-4xl text-purple-500 transition-transform duration-200 sm:text-5xl md:text-6xl hover:font-bold hover:scale-105">
+              Hi, I'm Katherine Chacon Cai!</p>
             <p className="text-2xl text-white sm:text-3xl md:text-4xl">
             I'm a UVA student majoring in Computer Science and minoring in English.
             </p>
@@ -14,16 +15,16 @@ const HomePage: React.FC = () => {
               and I can't wait to graduate. </p>
               <p className='text-xl text-white sm:text-2xl md:text-3xl'> If I've piqued your interest, here is my 
               
-              <a href={Resume} target="_blank" rel="noopener noreferrer" className ="text-blue-600"> resume</a>! 
+              <a href={Resume} target="_blank" rel="noopener noreferrer" className ="text-blue-600 hover:font-bold"> resume</a>! 
 
               Feel free to send me an email at
               
-              <a href="mailto:kqiancc@gmail.com" className ="text-blue-600"> kqiancc@gmail.com</a>, 
+              <a href="mailto:kqiancc@gmail.com" className ="text-blue-600 hover:font-bold"> kqiancc@gmail.com</a>, 
               
               or check out my 
               
-              <a href="https://github.com/kqiancc" target="_blank" rel="noopener noreferrer" className ="text-blue-600"> GitHub </a>
-                and <a href="https://www.linkedin.com/in/kqiancc" target="_blank" rel="noopener noreferrer" className ="text-blue-600">LinkedIn</a>.
+              <a href="https://github.com/kqiancc" target="_blank" rel="noopener noreferrer" className ="text-blue-600 hover:font-bold"> GitHub </a>
+                and <a href="https://www.linkedin.com/in/kqiancc" target="_blank" rel="noopener noreferrer" className ="text-blue-600 hover:font-bold">LinkedIn</a>.
               </p>
           </div>
         </div>
